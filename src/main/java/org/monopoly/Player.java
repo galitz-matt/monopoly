d60a8a2db902efdf105118fa522870394190eaca
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-02 12:51:44 -0400

    style(main): add newlines between method signatures in Player

--- src/main/java/org/monopoly/Player.java
+++ src/main/java/org/monopoly/Player.java
@@ -5,9 +5,14 @@ import java.util.List;
 public interface Player {
 
     void movePlayerTo(Tile destination);
+
     Tile getCurrentTile();
+
     int getBalance();
+
     void charge(int amount);
+
     void give(int amount);
+
     boolean isInJail();
 }
