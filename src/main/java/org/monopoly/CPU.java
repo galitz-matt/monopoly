42fac08d96cf608b4655cdad1bfbbf29c685ed4d
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-02 12:58:18 -0400

    feat(main): implement CPU realization of Player

---
+++ src/main/java/org/monopoly/CPU.java
@@ -0,0 +1,42 @@
+package org.monopoly;
+
+public class CPU implements Player {
+    private String name;
+    private Tile currentTile;
+    private int balance;
+    private boolean isInJail;
+    //TODO: create property manager field
+
+    public CPU(String name) {
+        this.name = name;
+        //TODO: set currentTile to first tile on GameBoard
+        this.balance = 0;
+    }
+    public void movePlayerTo(Tile destinationTile) {
+        currentTile = destinationTile;
+    }
+
+    public Tile getCurrentTile() {
+        return currentTile;
+    }
+
+    public int getBalance() {
+        return 0;
+    }
+
+    public void charge(int amount) {
+        if (amount > balance) {
+            //TODO: auto mortgage properties
+            //TODO: in Game class, check if player is bankrupt, if so remove from Game
+        }
+        balance -= amount;
+    }
+
+    public void give(int amount) {
+        balance += amount;
+    }
+
+    public boolean isInJail() {
+        return isInJail;
+    }
+}
