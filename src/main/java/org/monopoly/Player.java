2cca33958ef00b18abee2a6e73e33f9b80a503c2
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-02 12:34:57 -0400

    feat(main): write method signatures in Player, and create User realization

--- src/main/java/org/monopoly/Player.java
+++ src/main/java/org/monopoly/Player.java
@@ -1,7 +1,13 @@
 package org.monopoly;
 
-public interface Player {
-    
+import java.util.List;
 
-    
+public interface Player {
+
+    void movePlayerTo(Tile destination);
+    Tile getCurrentTile();
+    int getBalance();
+    void charge(int amount);
+    void give(int amount);
+    boolean isInJail();
 }
---
+++ src/main/java/org/monopoly/User.java
@@ -0,0 +1,28 @@
+package org.monopoly;
+
+public class User implements Player {
+
+    public void movePlayerTo(Tile destination) {
+
+    }
+
+    public Tile getCurrentTile() {
+        return null;
+    }
+
+    public int getBalance() {
+        return 0;
+    }
+
+    public void charge(int amount) {
+
+    }
+
+    public void give(int amount) {
+
+    }
+
+    public boolean isInJail() {
+        return false;
+    }
+}
