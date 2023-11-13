09e5247adca3b846d7d557129e7484c49541aa4d
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-12 20:34:44 -0500

    feat(main): create CommunityChest

---
+++ src/main/java/org/monopoly/CommunityChest.java
@@ -0,0 +1,32 @@
+package org.monopoly;
+
+import java.util.List;
+import java.util.Collections;
+
+public class CommunityChest {
+    private static CommunityChest instance;
+    private final List<Card> deck;
+    private int draws;
+    private int DECK_SIZE = 16;
+
+    private CommunityChest(JSONReader jsonReader) {
+        deck = jsonReader.getCards("communitychest");
+        draws = 0;
+    }
+
+    public CommunityChest getInstance() {
+        if (instance == null) {
+            instance = new CommunityChest(new JSONReader());
+        }
+        return instance;
+    }
+
+    public Card drawCard() {
+        if (draws % DECK_SIZE == 0) {
+            Collections.shuffle(deck);
+        }
+        draws++;
+        return deck.get(draws % DECK_SIZE);
+    }
+
+}
