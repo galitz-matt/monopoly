c2d138c32dbc80b86d80b24d518f076cc39a9135
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-12 20:37:27 -0500

    test(main): create CommunityChestTest

---
+++ src/test/java/CommunityChestTest.java
@@ -0,0 +1,11 @@
+import org.monopoly.CommunityChest;
+import org.monopoly.JSONReader;
+
+import org.junit.jupiter.api.Test;
+public class CommunityChestTest {
+
+    @Test
+    public void getCard() {
+        var communityChest = CommunityChest.getInstance();
+    }
+}
