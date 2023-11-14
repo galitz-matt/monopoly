3fb055f21df18aee8e04f7575e3e3749c3314844
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-14 14:17:20 -0500

    feat(main): create default method in Tile interface findDistanceToTile to replace GameBoard.findDistanceBetweenTiles (KEEP GAMEBOARD POJO)

--- src/main/java/org/monopoly/Tile.java
+++ src/main/java/org/monopoly/Tile.java
@@ -4,4 +4,9 @@ public interface Tile {
     void interact(Player player);
     String ID();
     int getPosition();
+
+    default int findDistanceToTile(Tile tile) {
+        var rawDistance = Math.abs(getPosition() - tile.getPosition());
+        return Math.min(rawDistance, 40 - rawDistance);
+    }
 }
