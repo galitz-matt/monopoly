ac7de4a830dfadaacfb9c05adfefc7c3179cb76f
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-14 18:49:18 -0500

    feat(main): implement CommunityChestTile.interact()

--- src/main/java/org/monopoly/CommunityChestTile.java
+++ src/main/java/org/monopoly/CommunityChestTile.java
@@ -3,13 +3,20 @@ package org.monopoly;
 public class CommunityChestTile implements Tile {
     private final String id;
     private final int position;
+    private static final CommunityChest communityChest = CommunityChest.getInstance();
 
     public CommunityChestTile(String id, int position) {
         this.id = id;
         this.position = position;
     }
     public void interact(Player player) {
-        //TODO: draw card and do action
+        System.out.printf("""
+                Current position %s
+                Drawing card...
+                """, StringStylist.bold(getName()));
+        var card = communityChest.drawCard();
+        System.out.printf("Your card says: %s\n", card.getPrompt());
+        card.interact(player);
     }
 
     public String ID() {
