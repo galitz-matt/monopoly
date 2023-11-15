8c2929ed7359ebb5976295d3b12b5fbcf2f3a15c
galitz-matt <94554253+galitz-matt@users.noreply.github.com>
2023-11-14 19:04:52 -0500

    feat(main): implement JailTile.interact()

--- src/main/java/org/monopoly/JailTile.java
+++ src/main/java/org/monopoly/JailTile.java
@@ -4,15 +4,10 @@ import java.util.Map;
 import java.util.HashMap;
 
 public class JailTile implements Tile {
-
-    private final Map<Player, Integer> inmates;
-
-    public JailTile() {
-        inmates = new HashMap<>();
-    }
-
     public void interact(Player player) {
-        //TODO: if in Jail, attempt to roll doubles, release player if stayed 3 turns
+        System.out.printf("""
+                Current Tile: %s
+                """, StringStylist.bold("Jail"));
     }
 
     public String ID() {
@@ -22,12 +17,4 @@ public class JailTile implements Tile {
     public String getName() { return "Jail"; }
 
     public int getPosition() { return 10; }
-
-    public void addInmate(Player player) {
-        inmates.put(player, 0);
-    }
-
-    public void incrementTime(Player player) {
-        inmates.put(player, inmates.get(player) + 1);
-    }
 }
